87f337064a7782c52e71ba3dbda3e4fb1d803cb0
Nathan Baker <nathanandrewbaker@gmail.com>
2020-07-04 20:32:46 -0700

    Update module documentation.
    Update __init__.py for modules and sub-modules.

---
+++ pdbx/__init__.py
@@ -0,0 +1,11 @@
+"""PDBx/mmCIF Python dictionary resources.
+
+All of the code in this repository is based on http://mmcif.wwpdb.org/.
+Specifically, this code is directly derived from
+http://mmcif.wwpdb.org/docs/sw-examples/python/src/pdbx.tar.gz linked from
+http://mmcif.wwpdb.org/docs/sw-examples/python/html/.
+
+See http://mmcif.wwpdb.org/docs/sw-examples/python/html/ for more information
+about this package, including examples.
+"""
+__all__ = ["reader", "writer"]
