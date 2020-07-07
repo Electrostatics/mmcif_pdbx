67a6a30da4ce68b5ff06fe657bde93f4f4f1f398
Thomas Holder <thomas.holder@schrodinger.com>
2020-07-07 20:04:12 +0200

    Refactor + improve pdbx.reader StopIteration usage
    - Don't throw StopIteration from tokenizer generator (PEP-479)
    - Generate useful exceptions for:
      - Unexpected EOF
      - Loop without values
    - Accept empty files
    
    Closes #22

--- tests/reader_test.py
+++ tests/reader_test.py
@@ -9,9 +9,11 @@
 #
 ##
 """Test cases for reading PDBx/mmCIF data files reader class."""
+import io
 import logging
 from pathlib import Path
 import pytest
+from pdbx.errors import PdbxSyntaxError
 from pdbx.reader import PdbxReader
 
 
@@ -40,3 +42,80 @@ def test_structure_factor_file(input_cif):
     container = container_list[0]
     refln_object = container.get_object("refln")
     assert refln_object is not None
+
+
+def read_cifstr(cifstr: str) -> list:
+    """Helper function"""
+    data = []
+    PdbxReader(io.StringIO(cifstr)).read(data)
+    return data
+
+
+def test_empty_file():
+    assert read_cifstr('') == []
+
+
+def test_empty_data_block():
+    assert read_cifstr('data_test')[0].get_object_name_list() == []
+
+
+def test_missing_value_eof():
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test _a.x')
+    assert "end of file" in str(excinfo.value)
+
+
+def test_missing_value_not_eof():
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test _a.x _a.y A')
+    assert "Missing data for item _a.x" in str(excinfo.value)
+
+
+def test_missing_key():
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test _a.x A B')
+    assert "Unrecognized syntax element: B" in str(excinfo.value)
+
+
+def test_empty_loop_header_eof():
+    # formal CIF 1.1 grammar expects at least one tag
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test loop_')
+    assert "end of file" in str(excinfo.value)
+
+
+def test_empty_loop_header_not_eof():
+    # formal CIF 1.1 grammar expects at least one tag
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test loop_ loop_')
+    assert "Unexpected token" in str(excinfo.value)
+
+
+def test_empty_loop_body_eof():
+    # formal CIF 1.1 grammar expects at least one value
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test loop_ _a.x')
+    assert "loop_ without values" in str(excinfo.value)
+
+
+def test_empty_loop_body_not_eof():
+    # formal CIF 1.1 grammar expects at least one value
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test loop_ _a.x loop_')
+    assert "Unexpected reserved word" in str(excinfo.value)
+
+
+def test_loop_value_count_mismatch():
+    # https://www.iucr.org/resources/cif/spec/version1.1/cifsyntax § 63:
+    # The number of values in the body must be a multiple of the number of tags in the header
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test loop_ _a.x _a.y A')
+        pytest.skip("does not raise yet")  # TODO
+
+
+def test_incomplete_multiline_string():
+    read_cifstr('data_test _a.x\n;A\n;')  # correct (terminated)
+    read_cifstr('data_test _a.x ;A')  # correct (not a multi-line string)
+    with pytest.raises(PdbxSyntaxError) as excinfo:
+        read_cifstr('data_test _a.x\n;A')
+    assert "unterminated multi-line" in str(excinfo.value)
