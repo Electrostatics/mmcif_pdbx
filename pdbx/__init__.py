f6c416dd36413b5f2eac26396b055db5a904456f
Thomas Holder <thomas.holder@schrodinger.com>
2020-07-08 09:41:02 +0200

    Add pdbx.load, pdbx.loads, pdbx.dump, pdbx.dumps
    Adding `json` and `pickle` inspired API.
    
    Related to #13

--- pdbx/__init__.py
+++ pdbx/__init__.py
@@ -8,3 +8,31 @@ http://mmcif.wwpdb.org/docs/sw-examples/python/html/.
 See http://mmcif.wwpdb.org/docs/sw-examples/python/html/ for more information
 about this package, including examples.
 """
+
+
+def load(fp) -> list:
+    """Parse a CIF file into a list of DataContainer objects"""
+    from .reader import PdbxReader
+    data = []
+    PdbxReader(fp).read(data)
+    return data
+
+
+def loads(s: str) -> list:
+    """Parse a CIF string into a list of DataContainer objects"""
+    import io
+    return load(io.StringIO(s))
+
+
+def dump(datacontainers: list, fp):
+    """Write a list of DataContainer objects to a CIF file"""
+    from .writer import PdbxWriter
+    PdbxWriter(fp).write(datacontainers)
+
+
+def dumps(datacontainers: list) -> str:
+    """Serialize a list of DataContainer objects to a CIF formatted string"""
+    import io
+    fp = io.StringIO()
+    dump(datacontainers, fp)
+    return fp.getvalue()
