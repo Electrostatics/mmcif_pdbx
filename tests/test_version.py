1af2109019a2f53d6419b6d9c7ca88cf775d558f
intendo <intendo23@gmail.com>
2021-03-27 22:44:47 -0700

    Testing version

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
 import re
 import pdbx
-from importlib import metadata
+from pdbx import __version__
 
 
 def test_version_exists():
@@ -8,7 +8,5 @@ def test_version_exists():
 
 
 def test_version():
-    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+", metadata.version("mmcif-pdbx"))
-    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+", metadata.version("mmcif_pdbx"))
-    print(f"VERSION1: {metadata.version('mmcif_pdbx')}")
-    print(f"VERSION2: {metadata.version('mmcif-pdbx')}")
+    assert re.match(r"[0-9]+\.[0-9]+\.[0-9]+", __version__)
+    print(f"VERSION: {__version__}")
